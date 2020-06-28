2b10b5269fefe176c6499fd306dc0197ff2a5127
Sounder Stark <sounderajendran@gmail.com>
2020-06-28 09:32:32 +0530

    Done Stark

--- minspy.py
+++ minspy.py
@@ -1,8 +1,11 @@
 import PySimpleGUI as sg
 import datetime
 import time
-import os
+import glob, os
 import keyboard
+from fpdf import FPDF 
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 today = datetime.date.today()
 d1 = today.strftime("%b_%d_%Y")
@@ -43,18 +46,15 @@ BORDER_COLOR = '#C7D5E0'
 BORDER_COLOR2 = '#787b80'
 
 DARK_HEADER_COLOR = '#1B2838'
-BPAD_TOP = ((20,20), (20, 10))
-BPAD_LEFT = ((20,10), (0, 10))
+BPAD_TOP = ((25,0), (20, 10))
+BPAD_LEFT = ((25,10), (0, 10))
 BPAD_LEFT_INSIDE = (0, 10)
-BPAD_LEFT2_INSIDE = (20, 10)
-BPAD_RIGHT = ((10,20), (10, 20))
-BPAD_BOTTOM = (100,0)
 
 d2 = today.strftime("%A, %b-%d-%Y  ")
 timenow = now = datetime.datetime.now()
 
-top_banner = [[sg.Text('Minute-inator'+ ' '*50, font='Any 20', background_color=DARK_HEADER_COLOR),
-               sg.Text(d2, font='Any 20', background_color=DARK_HEADER_COLOR), sg.Button(' X ')]]
+top_banner = [[sg.Text('   Minute-inator'+ ' '*35, font='Any 18', background_color=DARK_HEADER_COLOR),
+               sg.Text(d2, font='Any 18', background_color=DARK_HEADER_COLOR), sg.Button(' X ')]]
 
 clubname = "RaC Spectrum"
 clubid = "92268"
@@ -66,36 +66,30 @@ meetname = "Meeting Name Here"
 
 file_name = 'Minutes of ' + meetname + ' ' + today.strftime("%B_%d_%Y")+'.txt'
 
-
 detailstr = 'Host Club: ' + clubname + ' | Club ID: ' + clubid + ' | RID: ' + rid + ' | Chair: ' + chair + ' | Minutes by: ' + minsby
 
-top  = [[sg.Text('Meeting Name here', size=(50,1), justification='c', pad=BPAD_TOP, font='Any 20')],
-            [sg.Text(detailstr, size=(100,1), justification='c',  font='Any 10')], ]
+top  = [[sg.Text('Meeting Name here', size=(40,1), justification='c', pad=BPAD_TOP, font='Any 20')],
+            [sg.Text(detailstr, size=(109,1), justification='c',  font='Any 8')], ]
 
 block_2 = [[sg.Text('Auto Transcribe', font='Any 15')],
             [sg.Text('Speaker:', size = (10,1)), sg.InputCombo(values = attnlis, size=(20, 1),key='aspker'),sg.Button('Start Recording')] ,
             [sg.Text('', size = (10,1)),sg.Multiline(' ', size = (40,3), key ='autospeech')  ],[sg.Text(' ', size = (35,1)),sg.Button('Commit'),sg.Button('Enter')]]
 
-
-block_3 = [[sg.Text('Manual Minutes', font='Any 15')],
-            [sg.Text('Speaker:',size = (10,1)), sg.InputCombo(values = attnlis, size=(20, 1),key='manspker')],
-            [sg.Text('Points:', size = (10,1)), sg.Multiline(size=(40,3), key='manspeech')],[sg.Text(' ', size = (37,1)), sg.Button('Manual Enter')]]
-
 block_4 = [[sg.Text('Add Attendees', font='Any 15')],[sg.Text('Name:', size = (5,1)), sg.Input(size=(18,1),key='addattn')],[sg.Text(' ', size = (18,1)), sg.Button('Add',key='addattnbut')]]
 block_6 = [[sg.Text('Status Console', font='Any 15')],[sg.Text('Status: Initialised', size = (25,1),key='cons1')],[sg.Multiline('', size = (25,4),key='cons2')]]
 
-block_5 = [[sg.Text('Format + Save', font='Any 15')],
+block_3 = [[sg.Text('Format + Save', font='Any 15')],
             [sg.T('Select the .txt file to be converted to pdf')],
             [sg.Input(key = 'convert'), sg.FileBrowse()],
             [sg.Text(' ', size = (18,1)),sg.Button('Convert file'), sg.Button('Upload folder')],
            ]
 
-layout = [[sg.Column(top_banner, size=(960, 60), pad=(0,0), background_color=DARK_HEADER_COLOR)],
-          [sg.Column(top, size=(910, 90), pad=BPAD_TOP)],
+layout = [[sg.Column(top_banner, size=(740, 60), pad=(0,0), background_color=DARK_HEADER_COLOR)],
+          [sg.Column(top, size=(685, 90), pad=BPAD_TOP)],
           [sg.Column([[sg.Column(block_2, size=(440,160), pad=BPAD_LEFT_INSIDE)],
                       [sg.Column(block_3, size=(440,160),  pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT, background_color=BORDER_COLOR),
-           sg.Column([[sg.Column(block_4, size=(210,160), pad=BPAD_LEFT_INSIDE),sg.Column(block_6, size=(210,160), pad=BPAD_LEFT2_INSIDE)],
-                      [sg.Column(block_5, size=(440,160),  pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT, background_color=BORDER_COLOR),
+           sg.Column([[sg.Column(block_4, size=(210,160), pad=BPAD_LEFT_INSIDE)],
+                      [sg.Column(block_6, size=(210,160),  pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT, background_color=BORDER_COLOR),
            ]]
 
 window = sg.Window('Dashboard PySimpleGUI-Style', layout, margins=(0,0), background_color=BORDER_COLOR, no_titlebar=True, grab_anywhere=True)
@@ -126,19 +120,12 @@ while True:
         pass
 
     if event == 'Enter':
-        #manual writing of text from text box
+        #writing of text from text box
         window.Element('cons1').Update('Auto Enter Pressed')
         li = [values['aspker'] + ' at ' + str(timenow1) + ': ', values['autospeech']] 
         append_file(file_name, li)
         pass
         
-    if event == 'Manual Enter':
-        #manual writing of text from text box
-        window.Element('cons1').Update('Manual Enter Pressed')
-        li = [values['manspker'] + ' at ' + str(timenow1) + ': ', values['manspeech']] 
-        append_file(file_name, li)
-        pass
-
     if event == 'Commit':
         #publishing text from s2t.txt to multiline
         with open('s2t.txt') as ff:
@@ -154,10 +141,62 @@ while True:
         attnset=set(attnlis)
         attnlis=list(attnset)
         
-        window.Element('manspker').Update(values = attnlis)
         window.Element('aspker').Update(values = attnlis)
         
         pass
 
+    if event == 'Convert file':
+   
+        pdf = FPDF()    
+           
+        pdf.add_page() 
+           
+        pdf.set_font("times", size = 14) 
+
+        head, tail = os.path.split(values["convert"])  
+        f = open(tail, "r") 
+        
+        for x in f: 
+            pdf.cell(200, 10, txt = x, ln = 1, align = 'l') 
+           
+        pdf.output(tail[0:-4]+".pdf")    
+        print(tail[0:-4] + ".pdf" + " created")
+        window.Element('cons1').Update('Created' + ' ' + tail[0:-4] + '.pdf')
+        
+
+    if event == 'Upload folder':
+
+        #gdrive auth
+        g_login = GoogleAuth()
+        g_login.LocalWebserverAuth()
+        drive = GoogleDrive(g_login)
+        
+        with open(file_name,"r") as f:
+            fn = os.path.basename(f.name)
+            file_drive = drive.CreateFile({'title': fn })  
+        file_drive.Upload()
+        print ("The file: " + fn + " has been uploaded")
+
+        os.chdir("files_" + d1)
+
+        for file in glob.glob("*.wav"):
+          print (file)
+          with open(file,"r") as f:
+            fn = os.path.basename(f.name)
+            file_drive = drive.CreateFile({'title': fn })  
+          file_drive.Upload()
+          print ("The file: " + fn + " has been uploaded")
+
+        try:          
+            with open(file_name[:-4] + ".pdf","r") as f:
+                fn = os.path.basename(f.name)
+                file_drive = drive.CreateFile({'title': fn })  
+            file_drive.Upload()
+            print ("The file: " + fn + " has been uploaded")
+        except:
+            print ("No PDF was found under name" + fn )
+
+        window.Element('cons1').Update('Uploads Finished')
+        
 print("Closing Application")
 window.close()
